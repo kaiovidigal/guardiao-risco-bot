c2b9cade45b88a284bea9f930e232e89a3ffae7a
kaiovidigal <marechal.consultor@gmail.com>
2025-09-15 00:02:04 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 # main.py
 from fastapi import FastAPI
-from . import api_fanta  # Importa seu módulo api_fanta.py
+from . import api_fanta
 
 app = FastAPI()
 
 @app.get("/")
 async def get_fantan_result():
-    """
-    Endpoint que retorna o último resultado do Fan-Tan.
-    """
     result = await api_fanta.get_latest_result()
     if result:
         return {"numero": result[0], "timestamp": result[1]}
